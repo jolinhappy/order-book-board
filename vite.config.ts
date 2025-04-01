91aa504f4ee97c6928cf8f01cf9b94d7ab40f93a
Phoebe <jolinhappy1990@hotmail.com>
2025-04-01 20:39:07 +0800

    feat: add eslint and type checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,js}"',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      SRC: fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
