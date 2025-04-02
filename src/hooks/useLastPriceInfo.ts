b2193eb831c8d445217b4651d27c40c620c26310
Phoebe <jolinhappy1990@hotmail.com>
2025-04-03 00:05:04 +0800

    feat: adjust hooks

--- src/hooks/useLastPriceInfo.ts
+++ src/hooks/useLastPriceInfo.ts
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import useWebSocket from './useWebsocket';
-import { PriceInfo } from '@/types/order';
+import { PriceInfo, PriceInfoResponse } from '@/types/order';
 
 const useLastPriceInfo = () => {
   const [lastPriceInfo, setLastPriceInfo] = useState<PriceInfo | null>(null);
 
+  const handleMessage = useCallback((data: PriceInfoResponse) => {
+    if (!data || !data.data || data.data.length === 0) return;
+    setLastPriceInfo(data.data[0]);
+  }, []);
+
   useWebSocket({
     url: 'wss://ws.btse.com/ws/futures',
     topic: 'tradeHistoryApi:BTCPFC',
-    onMessage: (data) => {
-      if (!data || !data.data || data.data.length === 0) return;
-      setLastPriceInfo(data.data[0]);
-    },
+    onMessage: handleMessage,
   });
 
   return lastPriceInfo;
--- src/hooks/useOrderData.ts
+++ src/hooks/useOrderData.ts
@@ -6,6 +6,7 @@ const useOrderData = () => {
   const [orderData, setOrderData] = useState<OrderData | null>(null);
 
   const handleMessage = useCallback((data: OrderDataResponse, reconnect: () => void) => {
+    if (!data || !data.data) return;
     if (data.data.seqNum - 1 !== data.data.prevSeqNum) {
       // 序號不連續，重新訂閱
       reconnect();
