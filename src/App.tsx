c619d123938fd97a900a5b4a8354f2dd21c66fe7
Phoebe <jolinhappy1990@hotmail.com>
2025-04-02 23:09:10 +0800

    feat: add OrderBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from 'react';
-import './App.css';
+import OrderBoard from '@/components/OrderBoard';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="flex ">d</div>
-      <h1 className="text-3l mb-4">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="w-full h-screen flex justify-center items-center">
+      <OrderBoard />
+    </div>
   );
 };
 
---
+++ src/components/OrderBoard.tsx
@@ -0,0 +1,28 @@
+import QuotesInfo from '@/components/QuotesInfo';
+import { QuoteType } from '@/types/order';
+import CurrentPriceInfo from '@/components/CurrentPriceInfo';
+
+const OrderBoard = () => {
+  return (
+    <div className="bg-black w-80">
+      <p className="py-1 px-3 font-bold border-b border-blue-divider">Order Book</p>
+      {/* title */}
+      <div className="flex text-gray text-xs py-1 px-3">
+        <div className="w-[29%]">
+          <p>Price (USD)</p>
+        </div>
+        <div className="w-[30%] flex justify-end">
+          <p>Size</p>
+        </div>
+        <div className="w-[41%] flex justify-end">
+          <p>Total</p>
+        </div>
+      </div>
+      <QuotesInfo type={QuoteType.ASKS} />
+      <CurrentPriceInfo />
+      <QuotesInfo type={QuoteType.BIDS} />
+    </div>
+  );
+};
+
+export default OrderBoard;
